ad4e08ce044580abb1ddb41e12951fe560d96b3f
bobiiii <babersdn@gmail.com>
2024-09-26 17:16:29 +0500

    unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true,
         domains: [
             'drive.google.com',
             'drive.usercontent.google.com',
