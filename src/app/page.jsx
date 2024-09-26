04d627710cda14a6ce65b57401bf52dd67593eb4
bobiiii <babersdn@gmail.com>
2024-09-27 02:54:28 +0500

    POPULAR TOUR COMMENTED

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -46,7 +46,8 @@ async function page() {
       <WebHeader />
       <Hero />
       <Search />
-      <PopularTour popularToursData={popularToursData?.data} />
+
+      {/* <PopularTour popularToursData={popularToursData?.data} /> */}
       <HomeCategoryOne data={firstCategory} />
       <HomeCategoryTwo data={secondCategory} />
       <HomeCategoryThree data={thirdCategory} />
