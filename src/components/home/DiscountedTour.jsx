ec241d126a279649c4bb50a7a87d121e9d90d1fd
usmanzaib356 <usmanzaib356@gmail.com>
2024-09-27 21:45:36 -0700

    fixed data

--- src/components/home/DiscountedTour.jsx
+++ src/components/home/DiscountedTour.jsx
@@ -10,6 +10,8 @@ import CatalogCardCarousel from '../catalog/CatalogCardCarousel';
 function DiscountedTour({ DiscountedTours }) {
 
     const initialTours = DiscountedTours?.slice(0, 4);
+    console.log("DiscountedTours", DiscountedTours);
+
 
     return (
         <MaxWidthWrapper className='flex flex-col sm:gap-y-12 gap-y-8 sm:pt-20 pt-12  sm:pb-5 pb-4 px-2' >
