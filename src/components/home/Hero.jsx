715bba9124cce8082af363844abe4467c9170bc3
hettobaal <hettobaal@gmail.com>
2024-10-10 20:38:07 +0500

    preload bg image

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import MaxWidthWrapper from '../reuseable/MaxWidthWrapper';
 
 function Hero() {
+    ReactDOM.preload('/images/testhero.webp', { as: 'image' })
+    ReactDOM.preload('/images/testheromob.webp', { as: 'image' })
+
     return (
         <section className=" w-full sm:h-[90vh] h-[80vh] bg-cover bg-center  bg-no-repeat relative  text-white sm:bg-[url('/images/testhero.webp')] bg-[url('/images/testheromob.webp')]" >
             <MaxWidthWrapper className='flex flex-col sm:gap-y-6 gap-y-4 justify-center items-center max-w-screen-lg mx-auto h-full   lg:px-0 md:px-8'>
