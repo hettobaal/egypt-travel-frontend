41a006c6a19ef0ce718dd030eb6cf2f9f2d2fb02
hettobaal <hettobaal@gmail.com>
2024-10-15 23:23:38 +0500

    editor

--- src/app/(AdminPanel)/editor/Tiptap.jsx
+++ src/app/(AdminPanel)/editor/Tiptap.jsx
@@ -2,6 +2,7 @@
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import { generateHTML } from '@tiptap/react'
 import { useEffect } from 'react'
 import { MenuBar } from './MenuBar'
 import Link from '@tiptap/extension-link'
@@ -27,23 +28,51 @@ extensions: [StarterKit
 
   })
 
-  console.log('Editor:', editor); // Check if the editor instance is initialized
-
-  useEffect(() => {
-    console.log('Editor:', editor); // Check if the editor instance is initialized
-  }, []);
 
   if (!editor) {
     return <p>Loading editor...</p>; // You can show a loading state while the editor is initializing
   }
 
 
+
+  const handleSubmit = async () => {
+    if (editor) {
+      // const content = editor.getHTML(); // Get the HTML content from the editor
+      const content = editor.getJSON(); // Get content in JSON format
+
+      console.log('Editor content:', content);
+
+      try {
+        const response = await fetch('http://localhost:4000/admin/blog/add-blog', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            
+            
+            content }), // Send the content in the body
+        });
+
+        const result = await response.json();
+        console.log('Response from backend:', result);
+      } catch (error) {
+        console.error('Error sending content to backend:', error);
+      }
+    }
+  }
+  const savedContent = editor.getJSON();
+  const htmlContent = generateHTML(savedContent, [StarterKit]);
   return (
   
 <div className='h-[500px] p-4 border border-yellow-100'>  
 
     <MenuBar editor={editor}/>
     <EditorContent  editor={editor} />
+    <button onClick={handleSubmit} className="mt-4 p-2 bg-blue-600 text-white rounded">Save Content</button>
+
+{htmlContent &&
+    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />}
   </div>
 
 )
--- src/app/(AdminPanel)/editor/page.jsx
+++ src/app/(AdminPanel)/editor/page.jsx
@@ -3,5 +3,8 @@ import './styles.scss'
 import Tiptap from './tiptap'
 
 export default function Home() {
-  return <div className='mt-200'><Tiptap /></div>
+  return <div className='mt-200'><Tiptap />
+  
+  
+  </div>
 }
