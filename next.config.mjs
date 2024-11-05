db180e11a9225fdc41bbb2bcedd81405676a9738
hettobaal <hettobaal@gmail.com>
2024-11-05 21:25:55 +0500

    image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-
-        domains: [
-            'tourbuckettest.s3.amazonaws.com',
-            'drive.google.com',
-            'drive.usercontent.google.com',
-            'lh3.googleusercontent.com',
-            'vps-650845.dogado-cloud.de',
-        ],
+         remotePatterns: [
+            {
+              protocol: "https",
+              hostname: "**",
+            },
+        ]
         // unoptimized: true,
     },
 };
