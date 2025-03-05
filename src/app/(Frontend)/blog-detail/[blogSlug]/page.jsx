a9ba6e87daaaa0842f18b587dccf13f79966dfea
hettobaal <hettobaal@gmail.com>
2025-03-05 16:57:41 +0500

    #

--- src/app/(Frontend)/blog-detail/[blogSlug]/page.jsx
+++ src/app/(Frontend)/blog-detail/[blogSlug]/page.jsx
@@ -82,11 +82,9 @@ export async function generateMetadata({ params }) {
 async function page({ params }) {
     const slug = params?.blogSlug;
     const blog = await getSingleBlog(slug)
-    console.log("getSingleBlog   ", blog.data[0].title);
 
     const bannerImage = blog?.data?.blogBannerImage
     const entireBlog = await getBlogs()
-    console.log("entireBlog length", entireBlog?.data?.length);
 
     const relatedBlogsData = entireBlog?.data?.filter((item) => item?.slug !== slug)
 
