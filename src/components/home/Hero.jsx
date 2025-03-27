ebc421426e5c199ee488d1a9ff0f9ad4525a934f
Usmanzaib356 <110809791+Usmanzaib356@users.noreply.github.com>
2025-03-27 12:22:38 +0500

    changed hero image

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function Hero() {
-    ReactDOM.preload('/images/background.jpg', { as: 'image' })
+    ReactDOM.preload('/images/background2.webp', { as: 'image' })
     ReactDOM.preload('/images/background.webp', { as: 'image' })
 
     return (
-        <section className=" w-full sm:h-[90vh] h-[80vh] bg-cover bg-center  bg-no-repeat relative  text-white sm:bg-[url('/images/background.webp')] bg-[url('/images/background.webp')]" >
+        <section className=" w-full sm:h-[90vh] h-[80vh] bg-cover bg-center  bg-no-repeat relative  text-white sm:bg-[url('/images/background2.webp')] bg-[url('/images/background2.webp')]" >
         </section >
     )
 }
